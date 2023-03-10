1ffe0715359b23c728a0295ca7e87bd51edaed7f
Maximiliano Zapata <maximilianozapata84@gmail.com>
2023-03-09 21:04:00 -0300

    Agregando Firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYj_Srw69aleFx4Jbm0OeqmcAwHrcT-4Q",
+  authDomain: "mary-delicatessen.firebaseapp.com",
+  projectId: "mary-delicatessen",
+  storageBucket: "mary-delicatessen.appspot.com",
+  messagingSenderId: "166251273593",
+  appId: "1:166251273593:web:ab1bc66971a38e31bf63a8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
